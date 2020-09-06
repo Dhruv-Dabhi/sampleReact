9491ec21279fb09f7073049f7f48564d6840acb9
Dhruv Dabhi <dhruvdabhi123@gmail.com>
2020-09-06 18:57:54 -0300

    sample commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './external.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" class="back">
+		<h1 class="center"> Dhruv Dabhi</h1><br/>
+		<a href="http://www.dhruvdabhi.com/" target="_blank"
+			class="btn btn-default button1 center" role="button">Website</a>
+		<a href="https://www.linkedin.com/in/dhruvdabhi/" target="_blank"
+			class="btn btn-default button1 center" role="button">LinkedIn</a>	
+		<br/>
+		<h3 class="center">My contact inoformation:<br/><br/></h3>
+		<h4 class="center"> Email: <a href = "mailto: DhruvDTech@gmail.com"> DhruvDTech@gmail.com</a></h4>
+		<h4 class="center">Phone: +1-(226)-998-4893</h4>
+   </div>
   );
 }
 
